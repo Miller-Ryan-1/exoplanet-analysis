2eb0d73ab51142fd33fd7860ecd066d6eeb16cd4
Ryan Miller <ryan.d.miller05@gmail.com>
2022-07-05 09:01:05 -0500

    Helper Functions

--- explore.py
+++ explore.py
@@ -18,19 +18,75 @@ def column_null_analyzer(df):
         dict_list.append({'':col,'num_rows_missing':col_nulls,'pct_rows_missing':col_null_percent})
     return pd.DataFrame(dict_list).set_index('').sort_values(by='num_rows_missing', ascending=False)
 
-# UNIVARIATE DISTRIBUTIONS
+# UNIVARIATE DISTRIBUTIONS -------------------------------------------------
 
 def univariate(df):
-num_cols = [col for col in df.columns if df[col].dtype != 'object']
-cat_cols = [col for col in df.columns if df[col].dtype == 'object']
+    num_cols = [col for col in df.columns if df[col].dtype != 'object']
+    cat_cols = [col for col in df.columns if df[col].dtype == 'object']
 
-for i in num_cols:
-    print(i)
-    plt.figure(figsize = (10,5))
-    plt.subplot(121)
-    sns.boxplot(y=df[i].values)
-    plt.subplot(122)
-    sns.histplot(x=df[i])
+    for i in num_cols:
+        print(i)
+        plt.figure(figsize = (10,5))
+        plt.subplot(121)
+        sns.boxplot(y=df[i].values)
+        plt.subplot(122)
+        sns.histplot(x=df[i])
+        plt.show()
+        print(df[i].value_counts())
+        print('\n-----\n')
+
+# BI/MULTI-VARIATE ---------------------------------------------------------
+
+def plot_variable_pairs(train):
+    '''
+    Takes in a cleaned and split (but not scaled or encoded) training dataset 
+    and outputs a heatmap and linear regression lines based on correlations. 
+    '''
+    # Create lists of categorical and continuous numerical columns
+    num_cols = [col for col in train.columns if train[col].dtype != 'object']
+    cat_cols = [col for col in train.columns if train[col].dtype == 'object']
+    
+    # Create a correlation matrix from the continuous numerical columns
+    df_num_cols = train[num_cols]
+    corr = df_num_cols.corr()
+
+    # Pass correlation matrix on to sns heatmap
+    plt.figure(figsize=(12,12))
+    sns.heatmap(corr, annot=True, cmap="flare", mask=np.triu(corr))
     plt.show()
-    print(df[i].value_counts())
-    print('\n-----\n')
+    
+    # Create lm plots for all numerical data
+    combos = list(combinations(num_cols,2))
+    for i in combos:
+        sns.lmplot(x=i[0],y=i[1],data=train, hue=cat_cols[0])
+        plt.show()
+
+def plot_categorical_and_continuous_vars(train):
+    '''
+    Takes in a cleaned and split (but not scaled or encoded) training dataset 
+    and outputs charts showing distributions for each of the categorical variables.
+    '''
+    # Create lists of categorical and continuous numerical columns
+    num_cols = [col for col in train.columns if train[col].dtype != 'object']
+    cat_cols = [col for col in train.columns if train[col].dtype == 'object']
+    
+    # Create 3x side-by-side categorial to continous numeric plots
+    for i in num_cols:
+        plt.figure(figsize = (18,6))
+        plt.subplot(1,3,1)
+        sns.boxplot(data = train, x=cat_cols[0], y=i)    
+        plt.subplot(1,3,2)
+        sns.violinplot(data = train, x=cat_cols[0], y=i)
+        plt.subplot(1,3,3)
+        sns.barplot(data = train, x=cat_cols[0], y=i)
+        plt.show()
+
+def plot_numerical_against_target(train,target):
+    '''
+    Plots all unscaled numerical features against target.
+    '''
+    num_cols = [col for col in train.columns if train[col].dtype != 'object']
+
+    for i in num_cols:
+        sns.relplot(data = train, x=i, y=target)
+        plt.show()
--- wrangle.py
+++ wrangle.py
@@ -56,3 +56,90 @@ def splitter(df, target = 'None', train_split_1 = .8, train_split_2 = .7, random
         train, validate = train_test_split(train, train_size = train_split_2, random_state = random_state, stratify = train[target])
         print(f'Train = {train.shape[0]} rows ({100*(train_split_1*train_split_2):.1f}%) | Validate = {validate.shape[0]} rows ({100*(train_split_1*(1-train_split_2)):.1f}%) | Test = {test.shape[0]} rows ({100*(1-train_split_1):.1f}%)')
         return train, validate, test    
+
+def initial_clean(df):
+    # Create Columns/Features
+    df['planet'] = df.hostname + '-' + df.pl_letter
+    df['multistar'] = np.where(df.sy_snum > 1, 1,0)
+
+    # Filter Columns
+    df = df[(df.discoverymethod == 'Transit') | (df.discoverymethod == 'Radial Velocity')] # Drops about 2% of rows, most of which had lots of nulls
+    df = df[df.pl_controv_flag == 0] # Drops the controversial planets which are small enough best not to deal with
+
+    # Drop Columns
+    df = df.drop(columns = ['discoverymethod', # Indicate drop reasons below
+                            'pl_controv_flag',
+                            'sy_snum',
+                            'cb_flag',
+                            'hostname',
+                            'glat',
+                            'glon',
+                            'sy_gaiamag',
+                            'pl_ratdor',
+                            'pl_dens',
+                            'pl_masse',
+                            'st_logg',
+                            'rowupdate', # Useful only to impute some of the observation values, not for further analysis
+                            'disc_year'])
+
+    # Rename Columns
+    df = df.rename(columns={'sy_pnum':'num_planets_in_sys','pl_orbper':'orbital_period','pl_rade':'y','st_teff':'star_temp','st_met':'metallicity','st_lum':'luminosity','st_age':'star_age','st_mass':'star_mass','st_dens':'star_density','st_rad':'star_radius','sy_dist':'star_distance_from_earth'})
+
+    # Create the new df *Note: This is what you would replace with the loop/function to impute values
+    df = df.groupby(df.planet).mean()
+
+    # Change Datatypes
+    df = df.astype({'num_planets_in_sys':'int','multistar':'int'})
+
+    # Add back in any lost columns
+    df['discovery_order'] = df.index.str[-1].map({'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9})
+
+    # Encode the target (into Cat and 1/0)
+    df['y'] = np.where(df['y'] > .8, np.where(df['y']< 1.15,'Earthlike','Not-Earthlike'),'Not-Earthlike')
+    df['y_encoded'] = np.where(df['y'] == 'Earthlike',1,0)
+
+    # Nuke the last of the nulls
+    df = df.dropna()
+
+    return df
+
+def final_clean(train,validate,test):
+    X_lt_train = train[['luminosity','star_temp']]
+    X_lt_validate = validate[['luminosity','star_temp']]
+    X_lt_test = test[['luminosity','star_temp']]
+
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(X_lt_train)
+
+    train['lt_cluster'] = kmeans.predict(X_lt_train)
+    validate['lt_cluster'] = kmeans.predict(X_lt_validate)
+    test['lt_cluster'] = kmeans.predict(X_lt_test)
+
+    train = train.drop(columns = ['luminosity','star_temp','star_mass','star_radius','star_distance_from_earth'])
+    validate = validate.drop(columns = ['luminosity','star_temp','star_mass','star_radius','star_distance_from_earth'])
+    test = test.drop(columns = ['luminosity','star_temp','star_mass','star_radius','star_distance_from_earth'])
+
+    scale_cols = ['orbital_period','star_density']
+
+    scaler = MinMaxScaler()
+    scaler.fit(train[scale_cols])
+
+    train_scaled_features = pd.DataFrame(scaler.transform(train[scale_cols]),columns=['scaled_orb_period','scaled_star_density']).set_index([train.index.values])
+    validate_scaled_features = pd.DataFrame(scaler.transform(validate[scale_cols]),columns=['scaled_orb_period','scaled_star_density']).set_index([validate.index.values])
+    test_scaled_features = pd.DataFrame(scaler.transform(test[scale_cols]),columns=['scaled_orb_period','scaled_star_density']).set_index([test.index.values])
+   
+    train_scaled = train.merge(train_scaled_features, left_index = True, right_index = True).drop(columns = scale_cols)
+    validate_scaled = validate.merge(validate_scaled_features, left_index = True, right_index = True).drop(columns = scale_cols)
+    test_scaled = test.merge(test_scaled_features, left_index = True, right_index = True).drop(columns = scale_cols)
+
+    X_train = train_scaled.drop(columns=['y','y_encoded'])
+    y_train = train_scaled.y
+
+    X_validate = validate_scaled.drop(columns=['y','y_encoded'])
+    y_validate = validate_scaled.y
+
+    X_test = test_scaled.drop(columns=['y','y_encoded'])
+    y_test = test_scaled.y
+
+
+    return train, X_train, y_train, X_validate, y_validate, X_test, y_test
